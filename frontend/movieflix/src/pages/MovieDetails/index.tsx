0caacaeee0fe62b2f2537510a7a3f58000380168
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-12-09 22:58:15 -0300

    feature for member profile to post reviews

--- frontend/movieflix/src/pages/MovieDetails/index.tsx
+++ frontend/movieflix/src/pages/MovieDetails/index.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
 
 import ButtonIcon from 'components/ButtonIcon';
 import CardReview from 'components/CardReview';
-import { requestBackend } from 'util/requests';
-import { Review } from 'types/review';
 import CardMovie from 'components/CardMovie';
+import { requestBackend } from 'util/requests';
+import { hasAnyRoles, isAuthenticated } from 'util/auth';
+import history from 'util/history';
+import { Review } from 'types/review';
 import { Movie } from 'types/movie';
 
 import './styles.css';
@@ -15,11 +18,35 @@ type UrlParams = {
   movieId: string;
 };
 
+type FormData = {
+  text: string;
+  movieId: number;
+};
+
 const MovieDetails = () => {
   const { movieId } = useParams<UrlParams>();
   const [movieReviews, setMovieReviews] = useState<Review[]>([]);
   const [movie, setMovie] = useState<Movie>();
   const [isLoading, setIsLoading] = useState(false);
+  const { register, handleSubmit, setValue } = useForm<FormData>();
+
+  const onSubmit = (formData: FormData) => {
+    const params: AxiosRequestConfig = {
+      method: 'POST',
+      url: `/reviews`,
+      withCredentials: true,
+      data: {
+        text: formData.text,
+        movieId: movieId,
+      },
+    };
+    
+    requestBackend(params).then((response) => {
+      setMovieReviews((movieReviews) => [...movieReviews, response.data]);
+      setValue("text", '')
+    });
+  };
+
 
   useEffect(() => {
     setIsLoading(true);
@@ -27,14 +54,20 @@ const MovieDetails = () => {
       url: `/movies/${movieId}/reviews`,
       withCredentials: true,
     };
-    requestBackend(params)
+    isAuthenticated() ? 
+    (
+      requestBackend(params)
       .then((response) => {
         setMovieReviews(response.data);
       })
       .finally(() => {
         setIsLoading(false);
-      });
+      })
+    ) : (
+      history.push("/")
+    );
   }, [movieId]);
+
 
   useEffect(() => {
     setIsLoading(true);
@@ -47,28 +80,35 @@ const MovieDetails = () => {
         setMovie(response.data);
       })
       .finally(() => {
-        setIsLoading(false);
+        setIsLoading(false);       
       });
   }, [movieId]);
+
 
   return (
     <>
       <div className="movie-details-container">
-        {isLoading ? <h3 className="text-white">Carregando...</h3> : movie && <CardMovie movie={movie}/>}
-        <div className="base-card card-rating-container">
-          <form className="card-rating-container-form">
-            <input
-              type="text"
-              placeholder="Deixe sua avaliação aqui"
-              className="form-control base-input"
-            />
+        {isLoading ? (
+          <h3 className="text-white">Carregando...</h3>
+        ) : (
+          movie && <CardMovie movie={movie} />
+        )}
+        {hasAnyRoles(['ROLE_MEMBER']) && (
+          <div className="base-card card-rating-container">
+            <form className="card-rating-container-form" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="text"
+                {...register('text')}
+                placeholder="Deixe sua avaliação aqui"
+                className="form-control base-input"
+              />
 
-            <div className="card-rating-container-form-button">
-              <ButtonIcon text="SALVAR AVALIAÇÃO" />
-            </div>
-          </form>
-        </div>
-
+              <div className="card-rating-container-form-button">
+                <ButtonIcon text="SALVAR AVALIAÇÃO" />
+              </div>
+            </form>
+          </div>
+        )}
         <div className="movie-details-reviews">
           {isLoading ? (
             <h3>Carregando...</h3>
